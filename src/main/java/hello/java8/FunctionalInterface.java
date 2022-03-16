89e241735e51f45f498ea03c3b8ce9944845d94b
SangWook <imcool2551@gmail.com>
2022-03-16 17:00:21 +0900

    functional interface & method reference

---
+++ src/main/java/hello/java8/FunctionalInterface.java
@@ -0,0 +1,34 @@
+package hello.java8;
+
+import java.util.function.*;
+
+public class FunctionalInterface {
+
+    public static void main(String[] args) {
+        // 1. Function
+        Function<Integer, Integer> plus10 = i -> i + 10;
+        Function<Integer, Integer> multiply2 = i -> i * 2;
+
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+        Function<Integer, Integer> plus10AndMultiply2 = plus10.andThen(multiply2);
+        System.out.println(multiply2AndPlus10.apply(2));
+        System.out.println(plus10AndMultiply2.apply(2));
+
+        // 2. Consumer
+        Consumer<Integer> printInteger = System.out::println;
+        printInteger.accept(2);
+
+        // 3. Supplier
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10.get());
+
+        // 4. Predicate
+        Predicate<String> startsWithA = (s) -> s.startsWith("A");
+        Predicate<Integer> isOdd = (n) -> n % 2 == 1;
+        System.out.println(startsWithA.test("ABCD"));
+        System.out.println(isOdd.test(5));
+
+        // 5. UnaryOperator
+        UnaryOperator<Integer> plus1 = i -> i + 1;
+    }
+}
---
+++ src/main/java/hello/java8/MethodReference.java
@@ -0,0 +1,35 @@
+package hello.java8;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class MethodReference {
+
+    public static void main(String[] args) {
+
+        // 1. 스태틱 메서드 레퍼런스
+        UnaryOperator<String> sayHi = Greeting::hi;
+        sayHi.apply("keesun");
+
+        // 2. 인스턴스 메서드 레퍼런스
+        Greeting greeting = new Greeting();
+        UnaryOperator<String> sayHello = greeting::hello;
+        sayHello.apply("keesun");
+
+        // 3. 생성자 레퍼런스
+        Supplier<Greeting> newGreeting = Greeting::new;
+        Greeting greeting2 = newGreeting.get();
+
+        // 4. 생성자 레퍼런스(2)
+        Function<String, Greeting> newGreeting2 = Greeting::new;
+        Greeting keesun = newGreeting2.apply("keesun");
+        System.out.println(keesun.getName());
+
+        // 5. 임의 객체의 메서드 레퍼런스
+        String[] names = {"keesun", "whiteship", "toby"};
+        Arrays.sort(names, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(names));
+    }
+}
